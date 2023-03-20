ef68226d291919c71214c980a5ded88596f38411
Miroslava Ten <miroslavaten@MacBook-Air-Miroslava.local>
2023-03-20 17:36:00 +0600

    add inputs

--- src/Pages/AuthorizationPage/RegisterPage.jsx
+++ src/Pages/AuthorizationPage/RegisterPage.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Register from "../../components/Auth/Register";
 
 const RegisterPage = () => {
   return (
     <div>
       REGISTER
+      <Register />
       <p>
         Already have an account? <Link to="/login">Sign in</Link>
       </p>
--- src/components/Auth/Register.jsx
+++ src/components/Auth/Register.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 const Register = () => {
-  return <div></div>;
+  return (
+    <div>
+      <input type="email" placeholder="email" />
+      <input type="password" placeholder="password" />
+      <input type="password" placeholder="confirm password" />
+      <input type="type" placeholder="user name" />
+    </div>
+  );
 };
 
 export default Register;
