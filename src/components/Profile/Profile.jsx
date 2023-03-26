ab6597c2f7a14936c79e719d5a575b3268451fcf
aminanaz1~ <aftaami@gmail.com>
2023-03-26 20:30:05 +0600

    feat: add profile and edit profile

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,5 +1,5 @@
-import { Button } from "@mui/material";
-import { useEffect } from "react";
+import { Button, TextField } from "@mui/material";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import {
@@ -13,38 +13,57 @@ function Profile() {
   const users = useSelector((state) => state.userProfile.users);
   const { status, error } = useSelector((state) => state.userProfile);
   const dispatch = useDispatch();
+  const [addBtn, setAddBtn] = useState(false);
+  const [newUser, setNewUser] = useState({});
 
   useEffect(() => {
     dispatch(getUsers());
   }, [dispatch]);
 
-  // useEffect(() => {
-  //   dispatch(getUserDetails({ id: 1 }));
-  // }, [users]);
-
-  // const user = useSelector((state) => state.users.user);
-  // console.log(users);
+  const handleChange = (e) => {
+    setNewUser({ ...newUser, [e.target.name]: e.target.value });
+  };
 
   return (
     <div className="Profile">
       {status === "loading" && <h2>Loading...</h2>}
       {error && <h2>An error occured: {error}</h2>}
       <button
-        onClick={() => {
-          dispatch(addUser({ id: 11, name: "sardor", surname: "timofey" }));
-        }}
+        // onClick={() => {
+        //   dispatch(addUser({ id: 11, name: "sardor", surname: "timofey" }));
+        // }}
+        onClick={() => setAddBtn(!addBtn)}
       >
         Add user
       </button>
 
+      {addBtn && (
+        <div>
+          <TextField value={newUser.name} name="name" onChange={handleChange} />
+          <TextField
+            value={newUser.surname}
+            name="surname"
+            onChange={handleChange}
+          />
+          <Button
+            onClick={() => {
+              dispatch(addUser(newUser));
+            }}
+          >
+            Submit
+          </Button>
+        </div>
+      )}
+
       {users.map((user) => (
         <div style={{ display: "flex" }}>
-          <h3 onClick={() => dispatch(deleteUser(user.id))} key={user.id}>
-            {user.name}
-          </h3>
+          <h3>{user.name}</h3>
           <Link to={`/profile-details/${user.id}`}>
             <Button>Details</Button>
           </Link>
+          <Button onClick={() => dispatch(deleteUser(user.id))} key={user.id}>
+            Delete
+          </Button>
         </div>
       ))}
     </div>
