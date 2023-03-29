dd1e4cc446059bf114e4983136c151501cb27fd9
Miroslava Ten <miroslavaten@MacBook-Air-Miroslava.local>
2023-03-30 00:47:12 +0600

    make token refresh

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -1,12 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authSlice from "../features/userSlice/authSlice";
 import userProfileSlice from "../features/userProfileSlice/userProfileSlice";
-import userSlice from "../features/userSlice/userSlice";
 
 export const store = configureStore({
   reducer: {
     userProfile: userProfileSlice,
-    user: userSlice,
     auth: authSlice,
   },
 });
